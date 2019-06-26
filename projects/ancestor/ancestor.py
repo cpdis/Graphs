f2ad1ee768a4828edba1cb80e2cfedb90381659a
Colin Dismuke <colin.dismuke@gmail.com>
2019-06-26 16:07:37 -0500

    Added Stack class to ancestors

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -12,3 +12,20 @@ A Stack class can be based on previous assignments as well as the DFS implementa
 earliest_ancestor method will need to be written to actually implement the logic provided
 in the problem statement.
 '''
+
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
